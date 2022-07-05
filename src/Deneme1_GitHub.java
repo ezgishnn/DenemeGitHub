5f0ef38d8b8d111482682fbf45b1b852b9ad4173
ezgishnn <kilinc.ezgi@hotmail.com>
2022-07-05 16:49:41 -0400

    new commit

--- src/Deneme1_GitHub.java
+++ src/Deneme1_GitHub.java
@@ -2,6 +2,7 @@ public class Deneme1_GitHub {
     public static void main(String[] args) {
         System.out.println("Heeeeey");
         System.out.println("heeey github");
+        System.out.println();
 
     }
 }
